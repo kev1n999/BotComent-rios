7a576f51592cdda30cdc38b19c1e5e0fc8288c5f
kev1n999 <sopratesta999@gmail.com>
2025-05-12 22:12:38 -0300

    first commit

---
+++ src/services/puppeteerService.ts
@@ -0,0 +1,31 @@
+import puppeteer, { Browser, Page } from "puppeteer";
+
+export class PuppeteerServices {
+    private browser!: Browser;
+    
+    constructor(public headless?: boolean) {};
+
+    // Método que inicia a instância do navegador
+    public async init(): Promise<void> {
+        if (!this.browser) {
+            if (!this.headless) this.headless = false;
+            this.browser = await puppeteer.launch({ headless: this.headless});
+        }    
+    };
+    
+    /**
+     * @returns {Page} - retorna uma nova instância de Page(nova página)
+     */
+    public async getPage(): Promise<Page> {
+        if (!this.browser) {
+            await this.init();
+        }
+
+        return this.browser.newPage();
+    }
+
+    // Método para encerrar/fechar a instância atual da página
+    public async close(): Promise<void> {
+        if (this.browser) await this.browser.close();
+    }
+};
