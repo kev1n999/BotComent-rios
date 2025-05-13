e4b5f581ce1fa0187953d7933456ca1c9989417e
kev1n999 <sopratesta999@gmail.com>
2025-05-13 18:34:25 -0300

    search video function

---
+++ src/tasks/search.ts
@@ -0,0 +1,7 @@
+export async function searchVideos(searchMethod: string) {
+    if (searchMethod.trim().toLowerCase() === "hashtag") {
+        
+    } else if (searchMethod.trim().toLowerCase() === "search") {
+
+    }
+};
